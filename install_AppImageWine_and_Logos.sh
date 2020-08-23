3621c2a464a09407e25b4826eab817760d2767b3
DanielDevBR <dados.linux@gmail.com>
2020-08-23 12:09:42 -0300

    removed workaround, and bump version v2.6-rc1

--- install_AppImageWine_and_Logos.sh
+++ install_AppImageWine_and_Logos.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # From https://github.com/ferion11/LogosLinuxInstaller
-export THIS_SCRIPT_VERSION="v2.6-rc0"
-
-# try to workaround some pipes issues, setting SIGPIPE disposition to SIG_IGN:
-trap '' PIPE
+export THIS_SCRIPT_VERSION="v2.6-rc1"
 
 # version of Logos from: https://wiki.logos.com/The_Logos_8_Beta_Program
 export LOGOS_URL="https://downloads.logoscdn.com/LBS8/Installer/8.15.0.0004/Logos-x86.msi"
