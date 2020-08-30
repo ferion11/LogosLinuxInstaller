fe004309bd74cd7cfe8665fae87d1d81eb49a62b
DanielDevBR <dados.linux@gmail.com>
2020-08-30 19:28:01 -0300

    bump release v2.11

--- install_AppImageWine_and_Logos.sh
+++ install_AppImageWine_and_Logos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # From https://github.com/ferion11/LogosLinuxInstaller
-export THIS_SCRIPT_VERSION="v2.11-rc10"
+export THIS_SCRIPT_VERSION="v2.11"
 
 # version of Logos from: https://wiki.logos.com/The_Logos_8_Beta_Program
 if [ -z "${LOGOS_URL}" ]; then export LOGOS_URL="https://downloads.logoscdn.com/LBS8/Installer/8.15.0.0004/Logos-x86.msi" ; fi
