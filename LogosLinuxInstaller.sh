f74fd706abf54f5df19883c61512b8d55f1dedd7
John Goodman M0RVJ <jg00dman@users.noreply.github.com>
2023-02-23 12:35:46 +0000

    Update LogosLinuxInstaller.sh
    Updated to latest installer

--- LogosLinuxInstaller.sh
+++ LogosLinuxInstaller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC2317
-LOGOS_RELEASE_VERSION="10.1.0.0056"
+LOGOS_RELEASE_VERSION="23.1.0.0033"
 LOGOS_SCRIPT_TITLE="Logos Linux Installer" # From https://github.com/ferion11/LogosLinuxInstaller
 export LOGOS_SCRIPT_AUTHOR="Ferion11, John Goodman, T. H. Wright"
 export LOGOS_SCRIPT_VERSION="${LOGOS_RELEASE_VERSION}-v6" # Script version to match FaithLife Product version.
